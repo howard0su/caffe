5d71c1e4085ac117713b37a303560f7edb3658db
Howard Su <howard0su@gmail.com>
2016-11-18 19:56:18 -0500

    Add inital version for tiger competion

---
+++ data/Tiger/create_label.py
@@ -0,0 +1,29 @@
+import json
+import os.path
+import random
+
+fp = open('training/label.idl', 'r')
+trainval = open('trainval.txt', 'w')
+valid = open('test.txt', 'w')
+
+s = fp.readline()
+while s != '':
+	result = json.loads(s)
+	name = result.keys()[0]
+	id = os.path.splitext(os.path.basename(name))[0]
+	lablefn = 'label/' + id + '.txt'
+	txtfile = open(lablefn, 'w')	
+	for entry in result[name] :
+		txtfile.write ('{4}, {0}, {1}, {2}, {3}\n'.format(entry[0], entry[1], entry[2], entry[3], entry[4]))
+	txtfile.close()
+
+	# 10% data used for validation
+	if random.randint(0, 9) == 5:	
+		valid.write ('{0} {1}\n'.format('training/'+name, lablefn))
+	else:
+		trainval.write ('{0} {1}\n'.format('training/'+name, lablefn))
+	s = fp.readline()
+trainval.close()
+valid.close()
+fp.close()
+
