e6bb4129ec7902ebf812df1820b6b20b6dcdd61c
Howard Su <howard0su@gmail.com>
2017-11-15 18:58:47 +0800

    We don't need boost anymore

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -4,11 +4,6 @@ set(Caffe_INCLUDE_DIRS "")
 set(Caffe_DEFINITIONS "")
 set(Caffe_COMPILE_OPTIONS "")
 
-# ---[ Boost
-find_package(Boost 1.54 REQUIRED COMPONENTS system thread filesystem)
-list(APPEND Caffe_INCLUDE_DIRS PUBLIC ${Boost_INCLUDE_DIRS})
-list(APPEND Caffe_LINKER_LIBS PUBLIC ${Boost_LIBRARIES})
-
 # ---[ Threads
 find_package(Threads REQUIRED)
 list(APPEND Caffe_LINKER_LIBS PRIVATE ${CMAKE_THREAD_LIBS_INIT})
